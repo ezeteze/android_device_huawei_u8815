f440802e48e8008ab01f9df436ec517ff9fcc200
ezeteze <785462@gmail.com>
2013-05-19 19:05:34 +0200

    AOKP tree.

--- cm.mk
+++ cm.mk
@@ -5,7 +5,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 PRODUCT_RELEASE_NAME := U8815
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/huawei/u8815/u8815.mk)
